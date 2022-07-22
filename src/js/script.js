078d1ed36c3d17db3fa0cb0113ce69ee453e7236
Kirill <fatykovkirill@gmail.com>
2022-07-22 16:29:36 +0500

    добавил scroll up

--- src/js/script.js
+++ src/js/script.js
@@ -84,4 +84,20 @@ $(document).ready(function () {
     validateForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
 });
